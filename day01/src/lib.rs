0a0fb68d3c2140eee09adb7ae10f2fa06387df75
Alex Munro <alexmunro91@gmail.com>
2020-12-02 02:13:57 +0000

    Day 1 part two

--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -1,6 +1,5 @@
 use aoc2020::parse;
 
-use std::iter::FromIterator;
 use std::collections::HashSet;
 use std::path::Path;
 use thiserror::Error;
@@ -10,6 +9,23 @@ fn product_of_2020_pair(entries: HashSet<usize>) -> Option<usize>{
 
     match first {
         Some(n) => Some(n * (2020 - n)),
+        None => None,
+    }
+}
+
+fn product_of_2020_triple(entries: HashSet<usize>) -> Option<usize>{
+    // Cartesian product of the entries set with itself
+    let mut zipped = entries
+                        .iter()
+                        .flat_map(|a| entries.iter().map(move |b| (a, b)))
+                        .filter(|(a,b)| a != b);
+
+
+    // The first check prevents unsigned int underflow
+    let first_pair = zipped.find(|(a, b)| 2020 > *a + *b && entries.contains(&(2020 - *a - *b)));
+
+    match first_pair {
+        Some((a, b)) => Some(a * b * (2020 - a - b)),
         None => None,
     }
 }
@@ -24,8 +40,14 @@ pub fn part1(input: &Path) -> Result<(), Error> {
     Ok(())
 }
 
-pub fn part2(_input: &Path) -> Result<(), Error> {
-    unimplemented!()
+pub fn part2(input: &Path) -> Result<(), Error> {
+    let entries: HashSet<usize> = parse(input)?.collect();
+
+    match product_of_2020_triple(entries) {
+        Some(n) => println!("{}", n),
+        None => println!("Solution not found.")
+    }
+    Ok(())
 }
 
 
@@ -34,10 +56,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn product_of_2020_entries() {
+    fn test_product_of_2020_pair() {
         let entries = [1721, 979, 366, 299, 675, 1456].iter().cloned().collect();
         let expected = Some(514579);
         assert_eq!(product_of_2020_pair(entries), expected);
+    }
+    
+    #[test]
+    fn test_product_of_2020_triple(){
+        let entries = [1721, 979, 366, 299, 675, 1456].iter().cloned().collect();
+        let expected = Some(241861950);
+        assert_eq!(product_of_2020_triple(entries), expected);
     }
 }
 
