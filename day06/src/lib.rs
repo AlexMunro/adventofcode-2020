7ffd698f0e450c9b2f726fffce749af112f378d9
Alex Munro <alexmunro91@gmail.com>
2020-12-06 17:18:39 +0000

    Day 6 part two

--- day06/src/lib.rs
+++ day06/src/lib.rs
@@ -12,6 +12,16 @@ fn total_yessed(group_response: String) -> usize {
         .count()
 }
 
+fn total_completely_yessed(group_response: String) -> usize {
+    let mut individual_responses = group_response.split_whitespace();
+
+    let first_response = individual_responses.next().unwrap();
+
+    first_response.chars().unique().filter(
+        |c| individual_responses.clone().all(|r| r.contains(*c))
+    ).count()
+}
+
 pub fn part1(input: &Path) -> Result<(), Error> {
     let group_responses_count: usize = parse_newline_sep::<String>(input)
                                         .unwrap()
@@ -21,8 +31,13 @@ pub fn part1(input: &Path) -> Result<(), Error> {
     Ok(())
 }
 
-pub fn part2(_input: &Path) -> Result<(), Error> {
-    unimplemented!()
+pub fn part2(input: &Path) -> Result<(), Error> {
+    let group_responses_count: usize = parse_newline_sep::<String>(input)
+                                        .unwrap()
+                                        .map(|group_response| total_completely_yessed(group_response))
+                                        .sum();
+    println!("The answer to part two is {}", group_responses_count);
+    Ok(())
 }
 
 #[derive(Debug, Error)]
@@ -39,4 +54,12 @@ fn test_total_yessed() {
     assert_eq!(total_yessed("abac".to_string()), 3);
     assert_eq!(total_yessed("a".to_string()), 1);
     assert_eq!(total_yessed("a\na".to_string()), 1);
+}
+
+#[test]
+fn test_total_completely_yessed(){
+    assert_eq!(total_completely_yessed("abc".to_string()), 3);
+    assert_eq!(total_completely_yessed("a\na".to_string()), 1);
+    assert_eq!(total_completely_yessed("a\nb".to_string()), 0);
+    assert_eq!(total_completely_yessed("b\nab".to_string()), 1);
 }
