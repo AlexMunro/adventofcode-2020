d3bb105019f603423c564591a1da87ce9a74bf63
Alex Munro <alexmunro91@gmail.com>
2020-12-14 01:41:55 +0000

    Day 13 part two: correct, but in a way that isn't useful for those with a human lifespan.

--- day13/src/lib.rs
+++ day13/src/lib.rs
@@ -14,6 +14,25 @@ fn earliest_departure(timestamp: usize, ids: Vec<usize>) -> usize {
     earliest_id * remaining_minutes(earliest_id)
 }
 
+fn earliest_contiguous_departure(id_list: String) -> usize {
+    let mut ids: Vec<(usize, usize)> = id_list
+        .split(",")
+        .enumerate()
+        .filter_map(|(x, y)| match y.parse::<usize>().ok() {
+            Some(n) => Some((x, n)),
+            None => None,
+        })
+        .collect();
+
+    // Largest ID first
+    ids.sort_by(|(_, id_a), (_, id_b)| id_b.cmp(id_a));
+
+    (1..)
+        .map(|n| (n * ids[0].1) - ids[0].0)
+        .find(|n| ids.iter().all(|(pos, id)| (n + pos) % id == 0))
+        .unwrap()
+}
+
 pub fn part1(input: &Path) -> Result<(), Error> {
     let mut input_iter = parse::<String>(input)?;
     let timestamp = input_iter.next().unwrap().parse()?;
@@ -31,8 +50,12 @@ pub fn part1(input: &Path) -> Result<(), Error> {
     Ok(())
 }
 
-pub fn part2(_input: &Path) -> Result<(), Error> {
-    unimplemented!()
+pub fn part2(input: &Path) -> Result<(), Error> {
+    println!(
+        "The answer to part two is {}",
+        earliest_contiguous_departure(parse(input)?.nth(1).unwrap())
+    );
+    Ok(())
 }
 
 #[derive(Debug, Error)]
@@ -49,4 +72,22 @@ fn test_earliest_departure() {
     let timestamp = 939;
     let ids = vec![7, 13, 59, 31, 19];
     assert_eq!(earliest_departure(timestamp, ids), 295);
+}
+
+#[test]
+fn test_earliest_contiguous_departures() {
+    let ids = "17,x,13,19".to_string();
+    assert_eq!(earliest_contiguous_departure(ids), 3417);
+
+    let ids = "67,7,59,61".to_string();
+    assert_eq!(earliest_contiguous_departure(ids), 754018);
+
+    let ids = "67,x,7,59,61".to_string();
+    assert_eq!(earliest_contiguous_departure(ids), 779210);
+
+    let ids = "67,7,x,59,61".to_string();
+    assert_eq!(earliest_contiguous_departure(ids), 1261476);
+
+    let ids = "1789,37,47,1889".to_string();
+    assert_eq!(earliest_contiguous_departure(ids), 1202161486);
 }
