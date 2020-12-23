6edc7d04d7c0611b1263151effd2623db467d9a9
Alex Munro <alexmunro91@gmail.com>
2020-12-23 03:44:25 +0000

    Day 18 part two

--- day18/src/lib.rs
+++ day18/src/lib.rs
@@ -31,13 +31,33 @@ fn evaluate_simple_expression(expression: String) -> usize {
     })
 }
 
+fn evaluate_advanced_simple_expression(expression: String) -> usize {
+    let mut tokens = expression.split_whitespace();
+    let mut token_stack = vec![];
+
+    loop {
+        match tokens.next() {
+            Some("*") => (),
+            Some("+") => {
+                let prev_value = token_stack.pop().unwrap();
+                let next_value = tokens.next().unwrap().parse::<usize>().unwrap();
+                token_stack.push(prev_value + next_value);
+            }
+            Some(val) => token_stack.push(val.parse::<usize>().unwrap()),
+            None => break
+        }
+    }
+
+    token_stack.iter().product()
+}
+
 #[derive(Debug)]
 struct Subexpression {
     result: usize,
     length: usize,    
 }
 
-fn simplify_and_evalute(expression: &String, start: usize, end: usize, subexpressions: &HashMap::<usize, Subexpression>) -> usize {
+fn simplify_and_evalute(expression: &String, start: usize, end: usize, subexpressions: &HashMap::<usize, Subexpression>, evaluator: &dyn Fn(String) -> usize) -> usize {
     let mut i = start;
     let mut simplified = String::new();
     let mut substr_iter = expression.chars().skip(start);
@@ -60,10 +80,10 @@ fn simplify_and_evalute(expression: &String, start: usize, end: usize, subexpres
             }
         }
     }
-    evaluate_simple_expression(simplified)
+    evaluator(simplified)
 }
 
-fn evaluate_expression(expression: String) -> usize {
+fn evaluate_expression(expression: String, evaluator: &dyn Fn (String) -> usize) -> usize {
     let mut subexpressions = HashMap::<usize, Subexpression>::new();
     let mut open_stack: Vec<usize> = vec![];
 
@@ -77,7 +97,7 @@ fn evaluate_expression(expression: String) -> usize {
                 subexpressions.insert(
                     open_pos,
                     Subexpression{
-                        result: simplify_and_evalute(&expression, open_pos + 1, n - 1, &subexpressions),
+                        result: simplify_and_evalute(&expression, open_pos + 1, n - 1, &subexpressions, evaluator),
                         length: n - open_pos + 1,
                     }
                 );
@@ -86,18 +106,21 @@ fn evaluate_expression(expression: String) -> usize {
         }
     }
 
-    simplify_and_evalute(&expression, 0, expression.len() - 1, &subexpressions)
+    simplify_and_evalute(&expression, 0, expression.len() - 1, &subexpressions, evaluator)
 }
 
 pub fn part1(input: &Path) -> Result<(), Error> {
-    let sum: usize = parse(input)?.take_while(|s| *s != "").map(|expression| evaluate_expression(expression)).sum();
+    let sum: usize = parse(input)?.take_while(|s| *s != "").map(|expression| evaluate_expression(expression, &evaluate_simple_expression)).sum();
 
     println!("The answer to part one is {}", sum);
     Ok(())
 }
 
-pub fn part2(_input: &Path) -> Result<(), Error> {
-    unimplemented!()
+pub fn part2(input: &Path) -> Result<(), Error> {
+    let sum: usize = parse(input)?.take_while(|s| *s != "").map(|expression| evaluate_expression(expression, &evaluate_advanced_simple_expression)).sum();
+
+    println!("The answer to part two is {}", sum);
+    Ok(())
 }
 
 #[derive(Debug, Error)]
@@ -108,27 +131,49 @@ pub enum Error {
 
 #[cfg(test)]
 #[test]
-fn test_evaluate_expression() {
+fn test_evaluate_expression_simple() {
     let example = "1 + 2 + 3".to_string();
-    assert_eq!(evaluate_expression(example), 6);
+    assert_eq!(evaluate_expression(example, &evaluate_simple_expression), 6);
 
     let example = "1 * 2 + 5".to_string();
-    assert_eq!(evaluate_expression(example), 7);
+    assert_eq!(evaluate_expression(example, &evaluate_simple_expression), 7);
 
     let example = "10 * 20 + 4".to_string();
-    assert_eq!(evaluate_expression(example), 204);
+    assert_eq!(evaluate_expression(example, &evaluate_simple_expression), 204);
 
     let example = "2 * 3 + (4 * 5)".to_string();
-    assert_eq!(evaluate_expression(example), 26);
+    assert_eq!(evaluate_expression(example, &evaluate_simple_expression), 26);
 
     let example = "5 + (8 * 3 + 9 + 3 * 4 * 3)".to_string();
-    assert_eq!(evaluate_expression(example), 437);
+    assert_eq!(evaluate_expression(example, &evaluate_simple_expression), 437);
 
     let example = "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))".to_string();
-    assert_eq!(evaluate_expression(example), 12240);
+    assert_eq!(evaluate_expression(example, &evaluate_simple_expression), 12240);
     
     let example = "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2".to_string();
-    assert_eq!(evaluate_expression(example), 13632);
+    assert_eq!(evaluate_expression(example, &evaluate_simple_expression), 13632);
+}
+
+#[test]
+fn test_evaluate_expression_advanced() {
+    let example = "1 + (2 * 3) + (4 * (5 + 6))".to_string();
+    assert_eq!(evaluate_expression(example, &evaluate_advanced_simple_expression), 51);
+
+    
+    let example = "2 * 3 + (4 * 5)".to_string();
+    assert_eq!(evaluate_expression(example, &evaluate_advanced_simple_expression), 46);
+
+    
+    let example = "5 + (8 * 3 + 9 + 3 * 4 * 3)".to_string();
+    assert_eq!(evaluate_expression(example, &evaluate_advanced_simple_expression), 1445);
+
+    
+    let example = "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))".to_string();
+    assert_eq!(evaluate_expression(example, &evaluate_advanced_simple_expression), 669060);
+
+    
+    let example = "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2".to_string();
+    assert_eq!(evaluate_expression(example, &evaluate_advanced_simple_expression), 23340);
 }
 
 #[test]
@@ -138,4 +183,13 @@ fn test_evaluate_simple_expression() {
 
     let example = "1 * 2 + 5".to_string();
     assert_eq!(evaluate_simple_expression(example), 7);
+}
+
+#[test]
+fn test_evaluate_advanced_simple_expression() {
+    let example = "1 + 2 * 3 + 4".to_string();
+    assert_eq!(evaluate_advanced_simple_expression(example), 21);
+
+    let example = "2 * 5 + 1".to_string();
+    assert_eq!(evaluate_advanced_simple_expression(example), 12);
 }
